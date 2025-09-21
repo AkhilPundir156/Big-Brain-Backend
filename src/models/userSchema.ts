00a86882d33b6dde9551781e87eae00bd5c5d9df
Aman <20001003018aman@dcrustm.org>
2025-09-21 18:23:42 +0530

    [Add] contact controller, Contact Schema and cron service

--- src/models/userSchema.ts
+++ src/models/userSchema.ts
@@ -19,7 +19,28 @@ const userSchema = new Schema({
     avatar_url: String,
 });
 
+const contactSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
 const userModel = mongoose.model('user',userSchema);
+const contactModel = mongoose.model('contact', contactSchema);
+export {contactModel};
 
 export default userModel;
 
