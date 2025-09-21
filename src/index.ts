6eff144015616323e98e416f10d358d48bb6b98f
akpu-odoo <akpu@odoo.com>
2025-09-21 20:03:33 +0530

    [FIX] big_brain:  fix cors

--- src/index.ts
+++ src/index.ts
@@ -27,8 +27,11 @@ app.use(
     cors({
         origin: process.env.CLIENT_URL,
         credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
+
 app.use(
     session({
         secret: process.env.JWT_SECRET as string,
