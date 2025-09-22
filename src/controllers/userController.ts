bd17f170a127521d3f2f3e16aa132760b31b2b9b
akpu-odoo <akpu@odoo.com>
2025-09-22 15:29:50 +0530

    [IMP] big_brain:  IMP performance

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -128,7 +128,7 @@ export const LogoutHandler = asyncHandler(
         res.clearCookie("token", {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         });
 
         return res.status(200).json({ msg: "User logged out" });
