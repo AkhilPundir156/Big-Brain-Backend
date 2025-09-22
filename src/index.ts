d7ed061c805c198c05e2d36c212e198e3fdbc1db
akpu-odoo <akpu@odoo.com>
2025-09-22 12:47:44 +0530

    [FIX] big_brain:  fix cors

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
     cors({
-        origin: "*",
+        origin: process.env.CLIENT_URL,
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
