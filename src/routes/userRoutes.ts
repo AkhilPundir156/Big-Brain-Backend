8ac11794898780e611527d37f6d8bf1c5f0b4271
Aman <20001003018aman@dcrustm.org>
2025-10-03 02:03:16 +0530

    Enhance

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import passport from "passport";
 
-import { LoginHandler, LogoutHandler, SignupHandler, contactUsHandler, googleLoginHandler, myProfileHandler, changeTheme } from "../controllers/userController.js";
+import { LoginHandler, LogoutHandler, SignupHandler, contactUsHandler, googleLoginHandler, myProfileHandler, changeThemeHandler } from "../controllers/userController.js";
 import { upload } from "../middleware/uploadFile.js";
 import { isAuthenticated } from "../middleware/isAuthenticated.js";
 
@@ -14,7 +14,7 @@ UserRouter.post("/signup", upload.single("avatar_image"), SignupHandler);
 UserRouter.get("/me", isAuthenticated, myProfileHandler);
 
 //Updating theme
-UserRouter.patch("/theme", isAuthenticated, changeTheme );
+UserRouter.patch("/theme", isAuthenticated, changeThemeHandler );
 
 // User login (cookie is set inside LoginHandler)
 UserRouter.post("/login", LoginHandler);
