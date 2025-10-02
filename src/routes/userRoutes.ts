53be5711a4f3430529f85145b0c7ec5ae866774b
Aman <20001003018aman@dcrustm.org>
2025-10-02 17:20:12 +0530

    update theme in db

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import passport from "passport";
 
-import { LoginHandler, LogoutHandler, SignupHandler, contactUsHandler, googleLoginHandler, myProfileHandler } from "../controllers/userController.js";
+import { LoginHandler, LogoutHandler, SignupHandler, contactUsHandler, googleLoginHandler, myProfileHandler, changeTheme } from "../controllers/userController.js";
 import { upload } from "../middleware/uploadFile.js";
 import { isAuthenticated } from "../middleware/isAuthenticated.js";
 
@@ -12,6 +12,9 @@ const UserRouter = Router();
 UserRouter.post("/signup", upload.single("avatar_image"), SignupHandler);
 
 UserRouter.get("/me", isAuthenticated, myProfileHandler);
+
+//Updating theme
+UserRouter.patch("/theme", isAuthenticated, changeTheme );
 
 // User login (cookie is set inside LoginHandler)
 UserRouter.post("/login", LoginHandler);
